0590b457091dc39dcae05baa20a9f8e5555478cc
David Seidenberg <davidseidenberg@gmail.com>
2018-04-05 12:26:25 -0700

    Start mongoose setup and schema

---
+++ challenge_2/database.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/checkout');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connection established');
+});
+
+const purchaseSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  email: String,
+  password: String,
+  address: {
+    line1: String,
+    line2: String,
+    city: String,
+    state: String,
+    zip: String,
+  },
+  phone: String,
+  creditcard: {
+    number: String,
+    expiry: String,
+    cvv: String,
+    billzip: String
+  }
+})
