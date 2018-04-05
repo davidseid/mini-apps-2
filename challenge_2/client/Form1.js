447decc4e7fa9db4ee72ca2223ae6717aa31bf7e
David Seidenberg <davidseidenberg@gmail.com>
2018-04-05 12:12:15 -0700

    Built input component and render form 1 inputs skeleton

--- challenge_2/client/Form1.js
+++ challenge_2/client/Form1.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Input from './Input.js';
+
 const Form1 = (props) => {
   return (
     <div>
-      Form1
+      <h3>Form 1</h3>
+      <Input fieldName={'name'} />
+      <Input fieldName={'email'} />
+      <Input fieldName={'password'} />
+      <button>Next</button>
     </div>
   )
 }
---
+++ challenge_2/client/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = ({fieldName}) => {
+  return (
+    <div>
+      {fieldName}
+      <input></input>
+    </div>
+  )
+}
+
+export default Input;
