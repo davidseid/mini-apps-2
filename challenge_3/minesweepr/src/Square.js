da28f05fbd8038a22de1089ea3982fa7a0338b3d
David Seidenberg <davidseidenberg@gmail.com>
2018-04-06 15:23:24 -0700

    Add red for bomb

--- challenge_3/minesweepr/src/Square.js
+++ challenge_3/minesweepr/src/Square.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 const Square = ({isHidden, isBomb, nearbyBombs, rowNum, colNum, revealSquare}) => {
-  if (isHidden || isBomb) {
+  if (isHidden) {
     return (
       <div className="hidden-square" onClick={() => {revealSquare(rowNum, colNum)}}>
+      </div>
+    )
+  } else if (!isHidden && isBomb) {
+    return (
+      <div className="bomb-square">
       </div>
     )
   } else if (!isBomb) {
